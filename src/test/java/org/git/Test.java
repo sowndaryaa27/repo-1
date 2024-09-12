e42bc5b2af0679c0bbccad074d92395259bf47be
sowndaryaa27 <sowndaryadevi27@gmail.com>
2024-09-12 19:41:26 +0530

    new

---
+++ src/test/java/org/git/Test.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class Test {
+	private void method1() {
+		// TODO Auto-generated method stub
+		System.out.println("a");
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+		System.out.println("b");
+
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+		System.out.println("c");
+
+	}
+
+}
